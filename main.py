ade232a156067a87e68d6005d8db0a35200715cc
Francisco <frmauro8@gmail.com>
2023-05-09 15:41:08 -0300

    implemetation bootstrap css

--- main.py
+++ main.py
@@ -23,22 +23,23 @@ def home():
         # print(soup.title)
         # print(soup.get_text())
 
-        codaService = CodaService(url_to_scrape, soup.title)
+        codaService = CodaService(url_to_scrape, soup.title, soup.get_text())
         resColumn = CodaService.getColumnValue(url_to_scrape)
-        #print(resColumn)
+        print(resColumn)
+        print(resColumn["items"]["id"])
         #print(len(resColumn["items"]))
 
         itemsLenght = len(resColumn["items"])
 
         if itemsLenght == 0:
             print("Não foi encontrado nenhuma url com essa valor")
-            res = CodaService.createRow(codaService)
+            #res = CodaService.createRow(codaService)
             print(f'Inserted 1 row')
 
 
         if itemsLenght > 0:
-            #print("foi encontrado a url com essa valor")
-            res = CodaService.updateTitle(codaService)
+            print("foi encontrado a url com essa valor")
+            #res = CodaService.update(codaService)
             #print(f'Updated row {res["id"]}')
             #print(request.form["url"])
         return render_template("home.html", result = url_to_scrape)
